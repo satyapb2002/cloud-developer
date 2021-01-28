80c32d0256aff6c5394b976348b6f8d25a6f0af5
Satya Prakash Biswal <sbiswal@tepper.cmu.edu>
2021-01-28 00:07:23 -0600

    api config added

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'fqjrjcq410'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-zvgieoe9.us.auth0.com',            // Auth0 domain
+  clientId: '6X1QUppyCHd1Sfxx6jIILogmmbnrTeKJ',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
